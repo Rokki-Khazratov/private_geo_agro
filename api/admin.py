06666b0868a20a267ce85484f7d1529abcfbb382
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-11-08 16:18:56 +0500

    permission

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
+from .models import CustomUser, Plantation, PlantationCoordinates, Region, District, Fruits
 
-# Register your models here.
+# Inline admin for PlantationCoordinates
+class PlantationCoordinatesInline(admin.StackedInline):
+    model = PlantationCoordinates
+    extra = 1  # Количество пустых полей для добавления координат
+
+class PlantationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'district', 'status', 'established_date')
+    search_fields = ('name', 'district__name')
+    list_filter = ('district', 'status')
+    inlines = [PlantationCoordinatesInline]  # Добавьте инлайн для координат
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "district":
+            if request.user.is_superuser:
+                # Если это суперпользователь, показываем все округа
+                return super().formfield_for_foreignkey(db_field, request, **kwargs)
+            else:
+                # Фильтруем округа, которые доступны текущему пользователю
+                kwargs["queryset"] = District.objects.filter(users=request.user)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs  # Суперпользователь видит все
+        # Ограничиваем видимость плантаций по округам
+        return qs.filter(district__users=request.user)
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff')
+    search_fields = ('username', 'email')
+    filter_horizontal = ('districts',)
+
+class PlantationCoordinatesAdmin(admin.ModelAdmin):
+    list_display = ('plantation', 'latitude', 'longitude')
+    search_fields = ('plantation__id',)
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region')
+    search_fields = ('name', 'region__name')
+    list_filter = ('region',)
+
+class FruitsAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+# Регистрация моделей в админке
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Plantation, PlantationAdmin)
+admin.site.register(PlantationCoordinates, PlantationCoordinatesAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(District, DistrictAdmin)
+admin.site.register(Fruits, FruitsAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# UTILS 
+class HealthStatus(models.TextChoices):
+    YAHSHI = 'yahshi', 'Yahshi'
+    ORTACHA = 'ortacha', 'Ortacha'
+    YOMON = 'yomon', 'Yomon'
+
+# MODELS
+class Region(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class District(models.Model):
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Fruits(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class Plantation(models.Model):
+    PLANTATION_TYPE = (
+        (1, "Uzumzorlar"),
+        (2, "Issiqxonalar"),
+        (3, "Bog'lar"),
+    )
+    name = models.CharField(max_length=50)
+    inn = models.IntegerField()
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    plantation_type = models.IntegerField(choices=PLANTATION_TYPE)
+    fruit_type = models.ManyToManyField(Fruits)
+    
+    status = models.CharField(
+        max_length=30, 
+        choices=HealthStatus.choices,
+        default=HealthStatus.YAHSHI 
+    )
+    established_date = models.DateField()
+    is_checked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Plantation {self.id}"
+
+class PlantationCoordinates(models.Model):
+    plantation = models.ForeignKey(Plantation, related_name='coordinates', on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f"Coordinates for Plantation {self.plantation.id}"
+
+# Добавление модели User
+class CustomUser(User):
+    districts = models.ManyToManyField(District, related_name='users', blank=True)
+
+    def has_permission_for_plantation(self, plantation):
+        """Проверяет, имеет ли пользователь доступ к плантации."""
+        return plantation.district in self.districts.all()
+
+    def __str__(self):
+        return self.username
